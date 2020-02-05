74180a0b68b69b38dbc735e0f71ec08b58f7259d
alecvipa <hebrit_626@hotmail.com>
2020-02-04 20:58:27 -0600

    uv with .git

---
+++ build/precache-manifest.efdc249c06f9e29f508f911d66290c8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b7e5e15d57dc5572686ecba5838376b",
+    "url": "/clickHarryPotter/index.html"
+  },
+  {
+    "revision": "4bad8de4166ad1ccdbad",
+    "url": "/clickHarryPotter/static/css/main.90c2bdfc.chunk.css"
+  },
+  {
+    "revision": "ea4c207152a4d5cdfab1",
+    "url": "/clickHarryPotter/static/js/2.9a16ac41.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/clickHarryPotter/static/js/2.9a16ac41.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4bad8de4166ad1ccdbad",
+    "url": "/clickHarryPotter/static/js/main.65c4ee87.chunk.js"
+  },
+  {
+    "revision": "e2c693b4e81698701299",
+    "url": "/clickHarryPotter/static/js/runtime-main.da82fd06.js"
+  }
+]);
